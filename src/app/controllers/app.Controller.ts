68ffc62f8c3fe06d8cb140f0cf9226edcb77e6c0
Neeraj Singh <neerajonetwo@gmail.com>
2022-07-09 02:50:09 +0530

    dummy data added

--- src/app/controllers/app.Controller.ts
+++ src/app/controllers/app.Controller.ts
@@ -8,7 +8,7 @@ const addUser = async (req: any, res: any) => {
   const newUser = new AppModel({
     name: 'Neeraj Singh',
     email: 'okneerajsingh@gmail.com',
-    phone: 7409116740,
+    phone: 7400000740,
     password: '324234csd430sjldk4'
   })
   newUser.save().then(() => console.log('New use added successfully.'))
